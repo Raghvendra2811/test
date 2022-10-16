3939b33f5422e0e970aeaa3fdc17e01899ecd11a
Raghvendra2811 <85832090+Raghvendra2811@users.noreply.github.com>
2022-10-17 00:04:48 +0530

    update

--- google/index.js
+++ google/index.js
@@ -25,7 +25,12 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
 // CLIENT  ID   1092670337867-qjglh9oog459psi4dkc2u90nepa8vpgp.apps.googleusercontent.com
 
 // CLIENT SECRET   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
